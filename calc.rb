c29321ed68398c4db293f2395b60f11652cf53be
Tom Allen <tommarkallen@gmail.com>
2015-10-03 16:20:32 +1300

    Initial Commit

---
+++ calc.rb
@@ -0,0 +1,14 @@
+# EXERCISE 2.5
+# puts 24 * 365
+# puts 24 * 356 * 60 * 10
+# puts (24 * 60 * 60) * (365 * 30 + 180)
+
+puts (1160 * 1000000 / ( 365 * 24 * 60 * 60)).to_s + ' ye'
+
+# puts '2' * 5  # kinda cool
+# var = 5
+# puts var.to_s + '2'
+# x = 'You\'re swell'
+# puts x + '\\'
+puts '13.13 is a lucky room'.to_f
+
